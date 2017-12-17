c3dafd2e074c694a0c7b08cb5a8e1dc9dffb6276
Jimmy Thomsen <jimmy@codemagic.dk>
2017-12-17 19:07:48 +0100

    Re-added data and configuration file

---
+++ data/SMAttributes.xml.php
@@ -0,0 +1,27 @@
+<?php exit(); ?>
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<database>
+	<entry key="SMGalleryColumns" value="1"/>
+	<entry key="SMGalleryRows" value="2"/>
+	<entry key="SMGalleryWidth" value=""/>
+	<entry key="SMGalleryHeight" value="180"/>
+	<entry key="SMGalleryPadding" value="3"/>
+	<entry key="SMContactRecipients" value="mail@example-domain.com"/>
+	<entry key="SMContactSubject" value="Website Contact Form"/>
+	<entry key="SMContactSuccessMessage" value="Thanks, we will get back to you shortly"/>
+	<entry key="SMImageMontageMinHeight" value="100"/>
+	<entry key="SMImageMontageMaxHeight" value="300"/>
+	<entry key="SMImageMontageMargin" value="0"/>
+	<entry key="SMImageMontageDisplayTitle" value="true"/>
+	<entry key="SMImageMontageDisplayImageTitle" value="false"/>
+	<entry key="SMImageMontageDisplayImageExif" value="false"/>
+	<entry key="SMImageMontageDisplayPicker" value="false"/>
+	<entry key="SMImageMontageShuffle" value="true"/>
+	<entry key="SMImageMontageSlideShowInterval" value="3000"/>
+	<entry key="SMEnvironmentSessionName/SitemagicCMS" value="SMSESSIONdf4de1f072338a2f"/>
+	<entry key="SMPagesAutoSeoUrlsVersion" value="20160828"/>
+	<entry key="SMAutoSeoUrlsCompleted" value="true"/>
+	<entry key="SMPagesSettingsUrlType" value="Filename"/>
+	<entry key="SMPagesSettingsSeoUrls" value="true"/>
+	<entry key="SMClientCacheKey" value="9fd6477cdbb7e0f4187b45baf3495c0c"/>
+</database>
---
+++ data/SMContact.xml.php
@@ -0,0 +1,8 @@
+<?php exit(); ?>
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<database>
+	<entry id="8ff89f59524a305b944b057aea658492" title="Name *" type="Textfield" position="1"/>
+	<entry id="cd855cc2a868fa2174a537e72e3abfe8" title="E-mail *" type="Email" position="2"/>
+	<entry id="f8fe77b10ebf8d9e88434b34a04e9fad" title="Message" type="Textbox" position="4"/>
+	<entry id="203d7907a68678186cc97fed082839d1" title="Phone" type="Textfield" position="3"/>
+</database>
