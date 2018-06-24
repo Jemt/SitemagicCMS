80c5775f7382f24ba17d2a931506d96c878c4e9b
Jimmy Thomsen <jimmy@codemagic.dk>
2018-06-24 13:18:44 +0200

    Cherry picked commit 44af41d37362a1db6f369acb1f64712053ed4419 from branch SMCMS-JSShop.
    Made adjustments to make it work with master branch.

--- base/SMDataSource.classes.php
+++ base/SMDataSource.classes.php
@@ -721,8 +721,8 @@ class SMDataSource implements SMIDataSource
 				// case sensitive name of an attribute from a case insensitive attribute.
 				$orderbyField = $this->getCaseSensitiveAttribute($entry, $orderbyInfo[0]);
 
-				$orderbyValue = (($orderbyField !== null) ? $entry->getAttribute($orderbyField) : null);
-				$orderData[$countOrder][] = (($orderbyValue !== null) ? $this->normalize($this->decode($orderbyValue)) : null);
+				$orderbyValue = $entry->getAttribute($orderbyField); // Empty string if attribute does not exist
+				$orderData[$countOrder][] = $this->normalize($this->decode($orderbyValue));
 			}
 		}
 
@@ -787,7 +787,7 @@ class SMDataSource implements SMIDataSource
 			if (strtolower($attr->name) === strtolower($attribute))
 				return $attr->name;
 
-		return null;
+		return $attribute; // Attribute does not exist, return as-is
 	}
 
 	private function limitEntries($entries, $limit, $offset)
