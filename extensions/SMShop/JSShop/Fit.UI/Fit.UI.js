1c192119ce697933ec8942edc54f5f00b6ae5298
Jimmy Thomsen <jimmy@codemagic.dk>
2018-07-18 19:38:40 +0200

    Upgraded Fit.UI from version 1.0.40 to  version 1.0.41

--- extensions/SMShop/JSShop/Fit.UI/Fit.UI.js
+++ extensions/SMShop/JSShop/Fit.UI/Fit.UI.js
@@ -475,7 +475,7 @@ Fit._internal =
 {
 	Core:
 	{
-		VersionInfo: { Major: 1, Minor: 0, Patch: 40 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
+		VersionInfo: { Major: 1, Minor: 0, Patch: 41 } // Do NOT modify format - version numbers are programmatically changed when releasing new versions - MUST be on a separate line!
 	}
 };
 
@@ -7223,13 +7223,12 @@ Fit.Template = function(refreshable) // http://fiddle.jshell.net/5sb97qtn/28/  -
 
 					// Notice: Placeholders to be replaced by DOM elements should only
 					// be declared once - a DOM element cannot be added multiple times!
-					// Theoretically placeholders for string values could be used multiple times, but
-					// for the sake of consistency we only replace the first occurrence in this case too.
+					// Placeholders for string values can on the other hand be declared multiple times.
 					newHtml = newHtml.replace("{[" + key + "]}", "<var class='FitTemplate' id='PH" + id + "'></var>");
 				}
 				else // String value
 				{
-					newHtml = newHtml.replace("{[" + key + "]}", obj);
+					newHtml = newHtml.replace(new RegExp("{\\[" + key + "\\]}", "g"), obj);
 				}
 			}
 		});
